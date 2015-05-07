55a5d8ca38e6b346c163af651e20e48e21ab1936
AnanyaKumar <skywalker94@gmail.com>
2015-05-07 17:50:03 -0400

    Add late days qsub

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 // test implementations
 #include "paren_match.h"
-// #include "mandelbrot.h"
+#include "mandelbrot.h"
 #include "parallel_sequence.h"
 #include "serial_sequence.h"
 #include "cluster.h"
@@ -15,19 +15,19 @@ int main (int argc, char **argv) {
   Cluster::init(&argc, &argv);
 
   // paren test
-  //test_paren_match(100000000);
+  // test_paren_match(100000000);
 
   // mandelbrot test
-  //test_mandelbrot();
+  test_mandelbrot();
 
-  {
-    auto identity = [](int i) { return i; };
+  // {
+  //   auto identity = [](int i) { return i; };
 
-    ParallelSequence<int> *s = new ParallelSequence<int>(identity, 100);
-    s->set(5, 2);
-    cout << s->get(99) << endl;
-    cout << s->get(5) << endl;
-  }
+  //   ParallelSequence<int> *s = new ParallelSequence<int>(identity, 100);
+  //   s->set(5, 2);
+  //   cout << s->get(99) << endl;
+  //   cout << s->get(5) << endl;
+  // }
 
   //auto combiner = [](int x, int y) { return x + y; };
   //s->scan(combiner, 0);
